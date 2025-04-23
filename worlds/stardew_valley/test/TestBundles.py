736a1bd0a85bc23005fccec2c2924a699f8adcbe
Alex Gilbert <alexgilbert@yahoo.com>
2025-04-22 21:56:47 -0400

    - Progress on Meme bundles logic

--- worlds/stardew_valley/test/TestBundles.py
+++ worlds/stardew_valley/test/TestBundles.py
@@ -1,9 +1,10 @@
 import unittest
 
 from . import SVTestBase
-from .. import BundleRandomization
+from .. import BundleRandomization, location_table
 from ..data.bundles_data.bundle_data import all_bundle_items_except_money, quality_crops_items_thematic, quality_foraging_items, quality_fish_items
-from ..options import BundlePlando
+from ..locations import LocationTags
+from ..options import BundlePlando, BundlePrice
 from ..strings.bundle_names import BundleName
 from ..strings.crop_names import Fruit
 from ..strings.quality_names import CropQuality, ForageQuality, FishQuality
@@ -87,4 +88,20 @@ class TestRemixedAnywhereBundles(SVTestBase):
         for bundle_name in self.fish_bundle_names:
             with self.subTest(f"{bundle_name}"):
                 self.assertIn(bundle_name, location_names)
+
+
+class TestMemeBundlesAreResolvable(SVTestBase):
+    options = {
+        BundleRandomization.internal_name: BundleRandomization.option_meme,
+        BundlePrice.internal_name: BundlePrice.option_maximum,
+    }
+
+    def test_can_complete_all_bundles_with_all_state(self):
+        location_names = {location for location in self.multiworld.get_locations()}
+        self.collect_everything()
+        for location_name in location_names:
+            if LocationTags.MEME_BUNDLE not in location_table[location_name].tags:
+                continue
+            with self.subTest(f"{location_name}"):
+                self.assert_can_reach_location(location_name)
 
