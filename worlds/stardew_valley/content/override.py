f5ba0cd5dfe5858133ae9df5c80c16907b641512
Alex Gilbert <alexgilbert@yahoo.com>
2026-07-07 10:47:19 -0400

    - Fixed the biggest bug ever (all other_requirements were getting lost in DR)

--- worlds/stardew_valley/content/override.py
+++ worlds/stardew_valley/content/override.py
@@ -6,9 +6,16 @@ def override(content: Any, **kwargs) -> Any:
 
     # Annotations contain only the fields, not the cached properties
     # So we just pop any field that isn't an annotation, assuming it's something illegal like a cached property
-    field_on_class = type(content).__annotations__
+    # We also have to grab the parent types from their annotations, if applicable, so things like `other_requirements` get brought along
+    # We do this through __mro__ because it gives the whole tree, instead of just the immediate parents
+    content_type = type(content)
+    type_hierarchy = content_type.__mro__
+    annotations_on_class = set()
+    for base_type in type_hierarchy:
+        base_annotations = getattr(base_type, "__annotations__", {})
+        annotations_on_class.update(base_annotations)
     for field in list(attributes.keys()):
-        if field not in field_on_class:
+        if field not in annotations_on_class:
             attributes.pop(field)
 
     attributes.update(kwargs)
