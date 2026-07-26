0a4f73bee482c35c0fe36fe1863b9016e7f0c70d
Alex Gilbert <alexgilbert@yahoo.com>
2026-07-25 20:45:07 -0400

    - Fixed extended family requiring the normal legendaries

--- worlds/stardew_valley/logic/special_order_logic.py
+++ worlds/stardew_valley/logic/special_order_logic.py
@@ -85,8 +85,9 @@ class SpecialOrderLogic(BaseLogic):
                                           (self.logic.money.can_spend_at(Region.saloon_shop, 205000) & self.logic.cooking.can_cook(Beverage.triple_shot_espresso)) &
                                           (self.logic.money.can_spend_at(Region.pierre_shop, 170000) & self.logic.cooking.can_cook(Meal.bread)),
                 SpecialOrder.qis_kindness: self.logic.gifts.can_give_loved_gifts_to_everyone(),
-                SpecialOrder.extended_family: self.logic.ability.can_fish_perfectly() & self.logic.has(Fish.angler) & self.logic.has(Fish.glacierfish) &
-                                              self.logic.has(Fish.crimsonfish) & self.logic.has(Fish.mutant_carp) & self.logic.has(Fish.legend),
+                SpecialOrder.extended_family: self.logic.ability.can_fish_perfectly() & self.logic.fishing.can_catch_fish(Fish.ms_angler) &
+                                              self.logic.fishing.can_catch_fish(Fish.glacierfish_jr) & self.logic.fishing.can_catch_fish(Fish.son_of_crimsonfish) &
+                                              self.logic.fishing.can_catch_fish(Fish.radioactive_carp) & self.logic.fishing.can_catch_fish(Fish.legend_ii),
                 SpecialOrder.danger_in_the_deep: self.logic.ability.can_mine_perfectly() & self.logic.mine.has_mine_elevator_to_floor(120),
                 SpecialOrder.skull_cavern_invasion: self.logic.ability.can_mine_perfectly_in_the_skull_cavern(),
                 SpecialOrder.qis_prismatic_grange: self.logic.has(Loot.bug_meat) &  # 100 Bug Meat
