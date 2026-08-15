efd7c8e7ea17baf83fe7f319d26b3872ee1e6bd6
Jouramie <16137441+Jouramie@users.noreply.github.com>
2026-08-15 10:37:07 -0400

    remove plando connections from preset test

--- worlds/stardew_valley/test/test_options/TestPresets.py
+++ worlds/stardew_valley/test/test_options/TestPresets.py
@@ -1,4 +1,5 @@
-from Options import PerGameCommonOptions, OptionSet, OptionDict, Removed
+from Options import OptionDict, OptionSet, PerGameCommonOptions, PlandoConnections, Removed
+
 from ...options import StardewValleyOptions
 from ...options.presets import sv_options_presets
 from ...test.bases import SVTestCase
@@ -6,11 +7,16 @@ from ...test.bases import SVTestCase
 
 class TestPresets(SVTestCase):
     def test_all_presets_explicitly_set_all_options(self):
-        all_option_names = {option_key for option_key in StardewValleyOptions.type_hints}
-        omitted_option_names = {option_key for option_key in PerGameCommonOptions.type_hints}
-        mandatory_option_names = {option_key for option_key in all_option_names
-                                  if option_key not in omitted_option_names and
-                                  not issubclass(StardewValleyOptions.type_hints[option_key], OptionSet | OptionDict | Removed)}
+        all_option_names = set(StardewValleyOptions.type_hints)
+        omitted_option_names = set(PerGameCommonOptions.type_hints)
+        mandatory_option_names = {
+            option_key
+            for option_key in all_option_names
+            if option_key not in omitted_option_names
+            and not issubclass(
+                StardewValleyOptions.type_hints[option_key], OptionSet | OptionDict | PlandoConnections | Removed
+            )
+        }
 
         for preset_name in sv_options_presets:
             with self.subTest(f"{preset_name}"):
